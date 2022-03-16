59b374b14d56d42538aed69052fd03832adb186c
wangxin <xinwang213z@gmail.com>
2022-03-16 21:43:41 +0000

    Snake Game

--- input.js
+++ input.js
@@ -1,10 +1,25 @@
+let connection;
+
 const handleUserInput = function (key) {
   if (key === "\u0003") {
     process.exit();
   }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
 };
-const setupInput = function () {
+const setupInput = function (conn) {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
