65b8706e8ed274189d93d578767699abb2255a2a
wangxin <xinwang213z@gmail.com>
2022-03-16 21:18:02 +0000

    Snake Game

--- client.js
+++ client.js
@@ -1,3 +1,27 @@
-const { connect } = require('./play')
+const net = require("net");
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541,
+  });
+  console.log("Connecting ...");
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Connection established");
+    console.log("Writting message to server ");
+    conn.write("Name: wxx");
+  });
 
-connect()
+  conn.on("data", (data) => {
+    console.log("data: ", data);
+  });
+
+  return conn;
+};
+
+// console.log("Connecting ...");
+// connect();
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,46 +1,7 @@
-const net = require("net");
-const { stdin } = require("process");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-  });
-  console.log("Connecting ...");
-  // interpret incoming data as text
+console.log("Connecting ...");
+connect();
 
-  const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    const handleUserInput = function (key) {
-      if (key === "\u0003") {
-        process.exit();
-      }
-    };
-    return stdin;
-  };
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("Connection established");
-    console.log("Writting message to server ");
-    conn.write("Name: wxx");
-    // // console.log("Sending instructions");
-    // console.log("Enter input");
-    // const stdin = setupInput();
-    // stdin.on('data',)
-  });
-
-  conn.on("data", (data) => {
-    console.log("data: ", data);
-  });
-
-  return conn;
-};
-
-// console.log("Connecting ...");
-// connect();
-
-module.exports = { connect };
+setupInput();
