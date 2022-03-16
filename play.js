316b3a7f21a9ab99f3a908af0cffb0cee5616ee1
wangxin <xinwang213z@gmail.com>
2022-03-16 20:25:54 +0000

    2

--- play.js
+++ play.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const { stdin } = require("process");
 
 // establishes a connection with the game server
 const connect = function () {
@@ -8,28 +9,30 @@ const connect = function () {
   });
   console.log("Connecting ...");
   // interpret incoming data as text
+
+  const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    const handleUserInput = function (key) {
+      if (key === "\u0003") {
+        process.exit();
+      }
+    };
+    return stdin;
+  };
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     console.log("Connection established");
     console.log("Writting message to server ");
     conn.write("Name: wxx");
-    console.log("Sending instructions");
-    // setTimeout(() => {
-    //   conn.write("Move: left");
-    // }, 2050);
-    // setTimeout(() => {
-    //   conn.write("Move: up");
-    // }, 2000);
-    // setTimeout(() => {
-    //   conn.write("Move: right");
-    // }, 2100);
-    // setTimeout(() => {
-    //   conn.write("Move: left");
-    // }, 2150);
-    // setInterval(() => {
-    //   conn.write("Move: up");
-    // }, 50);
+    // // console.log("Sending instructions");
+    // console.log("Enter input");
+    // const stdin = setupInput();
+    // stdin.on('data',)
   });
+
   conn.on("data", (data) => {
     console.log("data: ", data);
   });
